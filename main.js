3c6901c9509fbfbec25e3f7ec44f547eaef4e9fa
Blackalking <blacj2317@gmail.com>
2024-08-30 00:41:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -527,6 +527,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn?.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
+  const bio =
   await conn?.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
